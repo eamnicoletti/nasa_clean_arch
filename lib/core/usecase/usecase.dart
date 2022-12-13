9470099cf8bd55dad17860ef0e2c21772a791958
Emilio Nicoletti <nico@pop-os.localdomain>
2022-12-13 20:18:03 -0300

    create getSpaceMediaFromDate method in repository

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
-import '../errors/failure.dart';
+import '../errors/failures.dart';
 
 abstract class Usecase<Output, Input> {
   Future<Either<Failure, Output>> call(Input params);
--- test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
+++ test/features/domain/usecases/get_space_media_from_date_usecase_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:nasa_clean_arch/core/errors/failure.dart';
+import 'package:nasa_clean_arch/core/errors/failures.dart';
 import 'package:nasa_clean_arch/core/usecase/usecase.dart';
 import 'package:nasa_clean_arch/features/domain/entities/space_media_entity.dart';
 import 'package:nasa_clean_arch/features/domain/repositories/space_media_repository.dart';
